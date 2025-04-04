e5484bd132527c75a0f39cbbb303cee1b6fa808c
kuma <yeawon6566@gmail.com>
2025-04-04 17:44:55 +0900

    TIL : 0404 - window.SpeechRecognition, window.webkitSpeechRecognition 활용한 페이지 제작

--- c_js/app/assets/js/module/kakaobook.js
+++ c_js/app/assets/js/module/kakaobook.js
@@ -4,7 +4,7 @@ const API_KEY = 'KakaoAK cd071edfdaa8bae521ef800793875782';
 const api = async params => {
   const queryString = new URLSearchParams(params).toString();
   const response = await fetch(API_URL + queryString, {
-    headers: {Authorization: 'KakaoAK cd071edfdaa8bae521ef800793875782'}
+    headers: {Authorization: API_KEY}
   });
 
   return response.json();
